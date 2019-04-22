76a75a993ad0a63484380afdefe105163359fec0
Katarzyna Connell <katarzyna.connell@yahoo.ie>
2019-04-22 15:29:31 +0100

    add code to show the data with the column headers

--- dataset.py
+++ dataset.py
@@ -1,7 +1,20 @@
 # Kasia Connell, 2019
-# dataset
+# Dataset presentation with the column headers
+
+# Import datasets, numpy and pandas and iris dataset 
 from sklearn import datasets
-iris = datasets.load_iris()
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_iris
+data = datasets.load_iris()
 digits = datasets.load_digits()
 
-print(iris)
+# Check the types of the features and response
+type('data.data')
+type('data.target')
+
+df = pd.DataFrame(np.column_stack((data.data, data.target)), columns = data.feature_names+['target'])
+
+data1 = pd.DataFrame(data= np.c_[data['data'], data['target']],
+                 columns= list(data['feature_names']) + ['target'])
+print(data1)
