80599bd151c676fecb49e814e48d68abc752f014
Katarzyna Connell <katarzyna.connell@yahoo.ie>
2019-04-27 11:18:44 +0100

    Update to Scatter_Matrix code and Readme comments and references

--- 1.Dataset.py
+++ 1.Dataset.py
@@ -18,8 +18,8 @@ from sklearn.datasets import load_iris
 #Define Iris data as Panda Dataframe
 seadf = sns.load_dataset('iris')
 
-#Print Panda Dataframe
-print(seadf)
+#Print first 20 rows to see the format of data
+print(seadf.head(20))
 
 #To give a statistical summary about the dataset (mean, maximum, minimum and devation figures)
 #Code adopted from https://medium.com/codebagng/basic-analysis-of-the-iris-data-set-using-python-2995618a6342
@@ -39,5 +39,10 @@ seadf.hist()
 plt.show()
 
 # scatter plot matrix
+# Verbatim code from https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
 scatter_matrix(seadf)
+plt.show()
+
+sns.pairplot(hue='species', markers="<", data = seadf, palette=['r' , 'y', 'k'])
+plt.savefig(Scatter_Matrix.jpg)
 plt.show()
